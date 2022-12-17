3aac6fe7b23a8ebae32dac5b6c9146e99e8293ac
ieatn <ieatnubcake@gmail.com>
2022-12-16 18:15:34 -0800

    responsive, stacked images, toggle answers

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const q = document.querySelectorAll('.question')
-const ans = document.querySelectorAll('.answer')
-q.forEach(i => i.addEventListener('click', () => {
-    return
+const answer = document.querySelectorAll('.answer')
+const question = document.querySelectorAll('.question')
+// for each question, add a showanswer click event, pass in that specific question
+question.forEach(q => q.addEventListener('click', () => {
+    q.addEventListener('click', showAnswer(q))
 }))
+// 
+const showAnswer = (q) => {
+    // bold the question
+    q.style.color = 'red'
+    // toggle active on answer
+    answer.forEach(ans => ans.classList.toggle('active'))
+}
