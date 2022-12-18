98af98c57b606127ca295c9cde2c4a00da17a8de
ieatn <ieatnubcake@gmail.com>
2022-12-17 18:06:58 -0800

    added question and answer toggles

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const answer = document.querySelectorAll('.answer')
-const question = document.querySelectorAll('.question')
-// for each question, add a showanswer click event, pass in that specific question
-question.forEach(q => q.addEventListener('click', () => {
-    q.addEventListener('click', showAnswer(q))
-}))
-// 
-const showAnswer = (q) => {
-    // bold the question
-    q.style.color = 'red'
-    // toggle active on answer
-    answer.forEach(ans => ans.classList.toggle('active'))
-}
+// OH MY GOD CHATGPT IS INCREDIBLE! ASTOUNDING! 
+// copied all this code from chatgpt and it works perfectly the future is now
+const questions = document.querySelectorAll('.question');
+questions.forEach(question => {
+  question.addEventListener('click', () => {
+    // Inside the event listener, use the nextElementSibling property to find the next sibling element of the question element, which should be the answer element:
+    const answer = question.nextElementSibling;
+    answer.classList.toggle('hidden');
+    // red color for question when clicked
+    question.classList.toggle('active')
+  });
+});
